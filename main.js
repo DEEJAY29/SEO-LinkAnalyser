ab7ba520a5d0449a0866815f9883cd93c7156419
Dhruv Jain <dhruvj797@gmail.com>
2022-11-03 22:35:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+console.log(process.argv);
+
+
+function main(){
+    if(process.argv.length>3){
+        console.log("More than one arguments passed")
+    }
+    if(process.argv.length>3){
+        console.log("No argument passed")
+    }
+    const requrl=process.argv[2]
+    console.log(`Now Crwaling on ${requrl}`)
+}
+
+main()
